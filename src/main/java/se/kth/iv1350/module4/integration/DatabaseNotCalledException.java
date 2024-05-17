a5bb667aa91fa13e65ae3ba5acb0d79736045564
zejoant <77977238+zejoant@users.noreply.github.com>
2024-05-17 16:25:22 +0200

    comment on exceptions

--- src/main/java/se/kth/iv1350/module4/integration/DatabaseNotCalledException.java
+++ src/main/java/se/kth/iv1350/module4/integration/DatabaseNotCalledException.java
@@ -1,15 +1,23 @@
 package se.kth.iv1350.module4.integration;
 
 /**
- *
+ *Exception that occurs if the program could not reach the inventory system
  * @author ludwigflodin
  */
 public class DatabaseNotCalledException extends Exception {
     
+    /**
+     * Exception message for the user
+     * @return message
+     */
     public String printExceptionMsg(){
         return "Servers are down, oopsie, try again!";
     }
     
+    /**
+     * Exception message for the developer
+     * @return message
+     */
     public String printAdminMsg(){
         return "Database error - " + java.time.LocalDateTime.now() 
                + "\nDatabase for the inventory system cannot be reached\n" 
--- src/main/java/se/kth/iv1350/module4/integration/ItemNotFoundException.java
+++ src/main/java/se/kth/iv1350/module4/integration/ItemNotFoundException.java
@@ -1,7 +1,7 @@
 package se.kth.iv1350.module4.integration;
 
 /**
- *
+ *Exception that occurs if the item was not found in the inventory system
  * @author ludwigflodin, antonhammar
  */
 public class ItemNotFoundException extends Exception {
@@ -12,10 +12,18 @@ public class ItemNotFoundException extends Exception {
         this.ID = ID;
     }
     
+    /**
+     * Exception message for the user
+     * @return message
+     */
     public String printExceptionMsg(){
         return "Item with identifier: (" + ID + ") not found";
     }
     
+    /**
+     * Exception message for the developer
+     * @return message
+     */
     public String printAdminMsg(){
         return "Item error - " + java.time.LocalDateTime.now() 
                + "\nIncorrect identifier used (" + ID+ ")\n" 
